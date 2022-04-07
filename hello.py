83d1f48083c62a78932ad914ed7f4fb7309c2224
Noah Gift <noah.gift@gmail.com>
2022-04-07 13:39:29 +0000

    adding requirements.txt

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 from random import choices
+import fire
 
-
-def fruit_generator(fruits):
+def fruit_generator(fruit):
     """Returns back random fruit"""
 
+    fruits = ["apple", "cherry"]
+    fruits.append(fruit)
+    print(f"You added this fruit {fruit}")
+    print(f"Randomly selecting from all fruits {fruits}")
     return choices(fruits)
 
 
-print(fruit_generator(["apple", "cherry", "peach"]))
+if __name__ == '__main__':
+    fire.Fire(fruit_generator)
